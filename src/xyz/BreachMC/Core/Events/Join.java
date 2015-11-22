1f669c370ab427965ad4c91c09eabb3afbc15a8c
Ronald Bunk <Experminator@users.noreply.github.com>
2015-11-22 13:42:07 +0100

    Update Join.java

--- src/xyz/BreachMC/Core/Events/Join.java
+++ src/xyz/BreachMC/Core/Events/Join.java
@@ -10,22 +10,16 @@ import xyz.BreachMC.Core.Head;
  * Created by Jesse on 21-11-2015.
  */
 public class Join implements Listener {
-
-    Head instance;
-    
-    public Join(Head head) {
-        this.instance = head;
-    }
     
     public void onLogin(PlayerLoginEvent event){
         if (event.getResult() == PlayerLoginEvent.Result.KICK_WHITELIST) {
-            event.setKickMessage(ChatColor.translateAlternateColorCodes('&', instance.getConfig().getString("Join.Whitelist")));
+            event.setKickMessage(ChatColor.translateAlternateColorCodes('&', Main.plugin.getConfig().getString("Join.Whitelist")));
         }
     }
 
     public void onJoin(PlayerJoinEvent e){
 
-        e.setJoinMessage(ChatColor.translateAlternateColorCodes('&', instance.getConfig().getString("Join.Join")));
+        e.setJoinMessage(ChatColor.translateAlternateColorCodes('&', Main.plugin.getConfig().getString("Join.Join")));
     }
 
     String replace(String text, String playername){
